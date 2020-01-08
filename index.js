9b6a0d35c807f5fde9c70b474d0ddd03e56d5a54
Goutham RG <gouthamrg97@gmail.com>
2020-01-09 00:30:10 +0530

    Updated Dependencies and Config

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+require('express-async-errors');
 const express = require('express');
 const app = express();
+const users = require('./routes/users');
+const mongoose = require('mongoose');
+const config = require('config');
+const auth = require('./routes/auth');
+
+if (!config.get('jwtPrivateKey')) {
+  throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
+}
+
+mongoose.connect('mongodb://localhost/ShareMoney', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => { console.log('Connected to db') })
+  .catch(() => { console.log('Error'); });
 
 app.use(express.json());
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
-app.get('/', (req, res) => {
-  res.send({ message: "Hello World!" });
-});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Share Money Application listening on port ${port}!`));
